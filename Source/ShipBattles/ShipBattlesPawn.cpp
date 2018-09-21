3d8bdac79083682ca5947a54075919ace312ec23
Skander <iskander9100@gmail.com>
2018-09-20 21:27:16 -0400

    JT pimping these bitches

--- Source/ShipBattles/ShipBattlesPawn.cpp
+++ Source/ShipBattles/ShipBattlesPawn.cpp
@@ -58,8 +58,8 @@ void AShipBattlesPawn::Tick(float DeltaSeconds)
 	// Calculate change in rotation this frame
 	FRotator DeltaRotation(0,0,0);
 	DeltaRotation.Pitch = CurrentPitchSpeed * DeltaSeconds;
-	DeltaRotation.Yaw = CurrentYawSpeed * DeltaSeconds;
-	DeltaRotation.Roll = CurrentRollSpeed * DeltaSeconds * 5.0f;
+	DeltaRotation.Yaw = CurrentYawSpeed * DeltaSeconds * 10.0f;
+	DeltaRotation.Roll = CurrentRollSpeed * DeltaSeconds;
 
 	// Rotate plane
 	AddActorLocalRotation(DeltaRotation);
